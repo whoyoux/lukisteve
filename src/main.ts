9fce1ba1ddb2e1e651384bd23ce8c4df8d7dc37a
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-05-01 16:39:26 +0200

    Added twitter og info

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
+const music = new Audio('muza.mp3');
+
 const main = () => {
     console.log('szkajpurPompa')
     console.log('jak cos to kopie wlasnie bitcoina na twoim kompie iksde')
-
-    //     const music = new Audio('muza.mp3');
-    //     music.loop = true;
-    //     music.play();
 }
 
 addEventListener('DOMContentLoaded', () => {
     main();
 })
+
+addEventListener('click', () => {
+    music.loop = true;
+    music.play();
+});
 
 export { }
