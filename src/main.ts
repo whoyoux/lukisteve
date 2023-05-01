14022ea9384c4c198a1e4fe2e5544eae05a80ec7
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-05-01 16:20:04 +0200

    new version

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,12 @@
-import "./style.css";
+const main = () => {
+    console.log('szkajpurPompa')
+    console.log('jak cos to kopie wlasnie bitcoina na twoim kompie iksde')
+
+    const music = new Audio('muza.mp3');
+    music.loop = true;
+    music.play();
+}
+
+addEventListener('DOMContentLoaded', () => {
+    main();
+})
