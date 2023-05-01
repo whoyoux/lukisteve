49a98b42d4471c0a8da5d3e62fa0ffdacfd81267
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-05-01 16:24:57 +0200

    workaround for chrome

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,9 @@ const main = () => {
     console.log('szkajpurPompa')
     console.log('jak cos to kopie wlasnie bitcoina na twoim kompie iksde')
 
-    const music = new Audio('muza.mp3');
-    music.loop = true;
-    music.play();
+    //     const music = new Audio('muza.mp3');
+    //     music.loop = true;
+    //     music.play();
 }
 
 addEventListener('DOMContentLoaded', () => {
