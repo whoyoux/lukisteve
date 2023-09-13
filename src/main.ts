d6e8e610e57cfa1333757051863547d95345d91c
whoyoux <48690153+whoyoux@users.noreply.github.com>
2023-09-13 20:41:51 +0200

    update

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,20 @@
 const music = new Audio('muza.mp3');
 
-const main = () => {
-    //console.log('szkajpurPompa')
-    //console.log('jak cos to kopie wlasnie bitcoina na twoim kompie iksde')
+// const main = () => {
 
-}
+// }
 
-addEventListener('DOMContentLoaded', () => {
-    main();
-})
+// addEventListener('DOMContentLoaded', () => {
+//     main();
+// })
 
 addEventListener('click', () => {
     music.loop = true;
     music.play();
+
+    window.scrollTo({
+        top: document.body.scrollHeight
+    });
 
     setInterval(() => {
         fetch('/').then(() => {
@@ -20,7 +22,6 @@ addEventListener('click', () => {
         })
 
     }, timeClamp(500, 2000));
-
 });
 
 const timeClamp = (min: number, max: number): number => {
@@ -36,15 +37,11 @@ class NewWindow {
         win.focus();
 
         setInterval(() => {
-
-
-            let w = Math.random() * parseInt(window.screen.availWidth.toString());
-            let h = Math.random() * parseInt(window.screen.availHeight.toString());
+            const w = Math.random() * parseInt(window.screen.availWidth.toString());
+            const h = Math.random() * parseInt(window.screen.availHeight.toString());
             win.resizeTo(w, h);
             win.moveTo(w, h)
 
-        }, 500);
+        }, 1000);
     }
 }
-
-export { }
